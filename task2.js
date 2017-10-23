e863a0a86738f6b916db798e9a21776598fab3f6
soo-park <sparkstacks@gmail.com>
2017-10-22 18:56:59 -0700

    brute force solution for task2 added

--- task2.js
+++ task2.js
@@ -4,6 +4,7 @@ person
 - email should not be empty
 - email whould validate one @ character
 - email should have one of 64 chars on both sides
+- a~z, A~Z, 0~9, . 
 - .@. valid
 - @exampl.com not valid
 
@@ -27,25 +28,85 @@ $('some_tag')
 */
 
 function solution() {
+  // write your code in JavaScript (Node.js 6.4.0)
+  //
+  // you can access DOM Tree using DOM Object Model:
+  //    document.getElementById
+  // or using jQuery:
+  //    $('some_tag')
+  //
+  // you can write to stdout for debugging purposes, e.g.
+  // console.log('this is a debug message');
+
+  var numbers = [];
+  for (var i = 0; i < 10; i++) {
+    numbers.push(i.toString());
+  }
 
   if($('#type_person').is(':checked')) {
-    console.log("person is checked");
-
     if ($('#first_name').val() === "" ||
         $('#last_name').val() === "" ||
         $('#email').val() === "") {
       return false;
     }
-    
+
+    // email validator
+    function emailValidator(email) {
+      var alphabet = 'abcdefghijklmnopqrstuvwxyz'
+      var alpha_lower = alphabet.split('');
+      var alpha_upper = alphabet.toUpperCase().split('');
+      var available = alpha_lower.concat(alpha_upper).concat(numbers).concat(["."]);
+      var emailSplit = email.split("@");
+
+      if(emailSplit.length === 2 &&
+        emailSplit[0].length !== 0 &&
+        emailSplit[1].length !== 0 &&
+        emailSplit[0].length <= 64 &&
+        emailSplit[1].length <= 64) {
+
+        emailSplit = emailSplit.join('');
+        for (let i = 0; i < emailSplit.length; i++) {
+          if (available.indexOf(emailSplit[i]) === -1) {
+            return false;
+          }
+        }
+      } else {
+        return false;
+      }
+      return true;
+    }
+
     // write email validation
-    console.log("email box value is ", $('#email').val())
+    return emailValidator($('#email').val());
 
   } else if ($('#type_company').is(':checked')) {
-    
-    console.log("company is checked");
-    console.log("company name box value is ", $('#company_name').val())
-    console.log("phone box value is ", $('#phone').val())
+    if ($('#company_name').val() === "" ||
+        $('#phone').val() === "") {
+      return false;
+    }
 
+    // phone number validator
+    function phoneNumberValidator(phone) {
+      var available = numbers.concat(["-", " "]);
+      var numberCounter = 0;
+      
+      for (let i = 0; i < phone.length; i++) {
+        if(available.indexOf(phone[i]) === -1) {
+          return false;
+        }
+        if (phone[i] !== "-" && phone[i] !== " " ) {
+          numberCounter++;
+        }
+      }
+
+      if (numberCounter < 6) {
+        return false;
+      }
+      return true;
+    }
+
+    // write phone validataion
+    return phoneNumberValidator($('#phone').val());
    }
 }
 
--- task3.js
+++ task3.js
@@ -1,11 +1,31 @@
-function solution(A) {
-  
-  for (let i = 0; i < A.length; i++) {
-      console.log(i);
-  }
+/*
+two non-negative numbers n and m
 
-  return "";
+similar if their decimal representation can be obtained from each other by rearranging their digits. 
+
+Note that a correct decimal representation does not contain leading zeroes
+
+1234 is similar to 2431
+1010 is similar to 1001
+12 is similar to 12
+123 is not similar to 234
+113 is not similar to 133
+100 is not similar to 10
+
+Input: non-negative integer N
+Output: number of non-negative integer similar to N
+*/
+
+
+function solution(N) {
+  
+  // make object of the number string with counter
+  // var NString = N.toString();
+  // for (let i = 0; i < NString.length; i++) {
+  //     console.log(NString[i]);
+  // }
+
+  return "a";
 }
 
-let input = [1, 3, 6, 4, 1, 2];
-console.log(solution(input));
+console.log(solution(100));
